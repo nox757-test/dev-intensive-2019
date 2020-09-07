f918f4f857b37bb2ce0c58e8bab0bc0496fee833
nox757-test <test2gweb@gmail.com>
2020-09-07 21:11:07 +0300

    Add hometask4 v0

---
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferenceRepository.kt
@@ -0,0 +1,52 @@
+package ru.skillbranch.devintensive.repositories
+
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import ru.skillbranch.devintensive.App
+import ru.skillbranch.devintensive.models.Profile
+
+object PreferenceRepository {
+
+    private const val FIRST_NAME = "FIRST_NAME"
+    private const val LAST_NAME = "LAST_NAME"
+    private const val ABOUT = "ABOUT"
+    private const val REPOSITORY = "REPOSITORY"
+    private const val RATING = "RATING"
+    private const val RESPECT = "RESPECT"
+
+    private val prefs: SharedPreferences by lazy {
+        val ctx = App.applicationContext()
+        PreferenceManager.getDefaultSharedPreferences(ctx)
+    }
+
+    fun getProfile(): Profile = Profile(
+            prefs.getString(FIRST_NAME, "")!!,
+            prefs.getString(LAST_NAME, "")!!,
+            prefs.getString(ABOUT, "")!!,
+            prefs.getString(REPOSITORY, "")!!,
+            prefs.getInt(RATING, 0),
+            prefs.getInt(RESPECT, 0)
+    )
+
+    fun saveProfile(profile: Profile) {
+        with(profile) {
+            putValue(FIRST_NAME to firstName)
+            putValue(LAST_NAME to lastName)
+            putValue(ABOUT to about)
+            putValue(RATING to repository)
+            putValue(RESPECT to rating)
+        }
+    }
+
+    private fun putValue(pair: Pair<String, Any>) = with(prefs.edit()) {
+        val key = pair.first
+        when (val value = pair.second) {
+            is String -> putString(key, value)
+            is Int -> putInt(key, value)
+            is Boolean -> putBoolean(key, value)
+            is Long -> putLong(key, value)
+            is Float -> putFloat(key, value)
+            else -> error("Only primitive types put prefs")
+        }
+    }
+}
